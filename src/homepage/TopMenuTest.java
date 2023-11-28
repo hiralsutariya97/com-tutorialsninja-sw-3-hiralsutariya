877ff5e0ea4ae4b107b78cedf81a197e53144314
hiralsutariya97 <hiralsutariya97@gmail.com>
2023-11-28 22:21:51 +0000

    tutorialninja

---
+++ src/homepage/TopMenuTest.java
@@ -0,0 +1,85 @@
+package homepage;
+/**
+ * create class "TopMenuTest"
+ * 1.1 create method with name "selectMenu" it has one parameter name "menu" of type
+ * string
+ * 1.2 This method should click on the menu whatever name is passed as parameter.
+ * Write the following Test:
+ * 1. verifyUserShouldNavigateToDesktopsPageSuccessfully()
+ * 1.1 Mouse hover on “Desktops” Tab and click
+ * 1.2 call selectMenu method and pass the menu = “Show All Desktops”
+ * 1.3 Verify the text ‘Desktops’
+ * 2. verifyUserShouldNavigateToLaptopsAndNotebooksPageSuccessfully()
+ * 2.1 Mouse hover on “Laptops & Notebooks” Tab and click
+ * 2.2 call selectMenu method and pass the menu = “Show All Laptops & Notebooks”
+ * 2.3 Verify the text ‘Laptops & Notebooks’
+ * 3. verifyUserShouldNavigateToComponentsPageSuccessfully()
+ * 3.1 Mouse hover on “Components” Tab and click
+ * 3.2 call selectMenu method and pass the menu = “Show All Components”
+ * 3.3 Verify the text ‘Components’
+ */
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utilities.Utility;
+
+public class TopMenuTest extends Utility {
+
+    String baseUrl = "http://tutorialsninja.com/demo/index.php?";
+
+    @Before
+    public void setUp() {
+
+        openBrowser(baseUrl);
+    }
+
+
+    @Test
+    public void verifyUserShouldNavigateToDesktopsPageSuccessfully()  {
+        //1.1 Mouse hover on "Desktops" tab and click
+        mouseHoverAndClick(By.xpath("//a[text()='Desktops']"));
+
+        //1.2 call selectMenu method and pass the menu = “Show All Desktops”
+        selectMenu("Show AllDesktops");
+
+
+        //1.3 Verify the text ‘Desktops’
+        Assert.assertEquals("Desktops", getTextFromElement(By.xpath("//h2[normalize-space()='Desktops']")));
+
+    }
+
+    @Test
+    public void verifyUserShouldNavigateToLaptopsAndNotebooksPageSuccessfully() throws InterruptedException {
+        //2.1 Mouse hover on “Laptops & Notebooks” Tab and click
+        mouseHoverAndClick(By.xpath("//a[@class='dropdown-toggle'][normalize-space()='Laptops & Notebooks']"));
+
+        //2.2 call selectMenu method and pass the menu = “Show All Laptops & Notebooks”
+        selectMenu("Show AllLaptops & Notebooks");
+
+        //2.3 Verify the text ‘Laptops & Notebooks’
+        Assert.assertEquals("Laptops & Notebooks", getTextFromElement(By.xpath("//h2[normalize-space()='Laptops & Notebooks']")));
+
+    }
+
+    @Test
+    public void verifyUserShouldNavigateToComponentsPageSuccessfully() {
+
+        //3.1 Mouse hover on “Components” Tab and click
+        mouseHoverAndClick(By.xpath("//a[normalize-space()='Components']"));
+
+
+        //3.2 call selectMenu method and pass the menu = “Show All Components”
+        selectMenu("Show AllComponents");
+
+        //3.3 Verify the text ‘Components’
+        Assert.assertEquals("Components", getTextFromElement(By.xpath("//h2[normalize-space()='Components']")));
+    }
+
+    @After
+    public void tearDown() {
+        closeBrowser();
+    }
+}
